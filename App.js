2ab840aaf69f1d095f5efcf88f4d1a53759d31dd
Alexander Ela <ela.alexander@gmail.com>
2019-04-06 12:40:58 -0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: 'pink',
+    borderWidth: 2,
+    margin: 10
+  },
+  text: {
+    color: 'red',
+    borderColor: 'black',
+    borderWidth: 1
+  },
+});
