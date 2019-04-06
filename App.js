c895d5f736b70d1662861c7b07c7abeae17e8f9f
Alexander Ela <ela.alexander@gmail.com>
2019-04-06 13:36:43 -0600

    Add text state property

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <TextInput 
+          style={styles.conceptInput}
+          onChangeText={(text) => this.setState({ text })}
+        />
+        <Text>{this.state.text}</Text>
       </View>
     );
   }
@@ -16,12 +27,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 100,
     borderColor: 'pink',
     borderWidth: 2,
     margin: 10
   },
-  text: {
+  conceptInput: {
+    height: 30,
+    width: 200,
+    fontSize: 30,
     color: 'red',
     borderColor: 'black',
     borderWidth: 2
